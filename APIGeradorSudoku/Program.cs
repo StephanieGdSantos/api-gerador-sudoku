159ec7cc47645117bd1b750073a8c5e652f66441
StephanieGdSantos <gomess214@gmail.com>
2026-01-17 22:40:00 -0300

    refactor: permite visualização do swagger independentemente do ambiente

--- APIGeradorSudoku/Program.cs
+++ APIGeradorSudoku/Program.cs
@@ -36,12 +36,8 @@ builder.Services.AddOptions<QuantidadeMaximaQuadradosEmBrancoPorNivelOptions>()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
